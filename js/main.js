70da03fe6e78727a738b7c0bf1385c79edd9dd30
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-07-30 12:26:55 +0300

    add free tasks

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function unique(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+
+function unique(arr) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+
+function charFrequency(str) {
+  let result = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (result[str[i]]) {
+      result[str[i]] += 1;
+    } else {
+      result[str[i]] = 1;
+    }
+  }
+
+  return result;
+}
+
+console.log(charFrequency("hello"));

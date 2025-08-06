d058d680b8ddff2ad6b6ec4e8a20c93761622ce3
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-06 11:12:18 +0300

    hoisting tasks

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
-function unique(arr) {
-  return [...new Set(arr)];
+// function unique(arr) {
+//   return [...new Set(arr)];
+// }
+
+// console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+
+// function unique(arr) {
+//   const result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!result.includes(arr[i])) {
+//       result.push(arr[i]);
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+
+// function charFrequency(str) {
+//   let result = {};
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (result[str[i]]) {
+//       result[str[i]] += 1;
+//     } else {
+//       result[str[i]] = 1;
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(charFrequency("hello"));
+
+// function isPalindrome(str) {
+//   const clearStr = str.toLowerCase().replace(/\s/g, "");
+
+//   const reversed = clearStr.split("").reverse().join("");
+
+//   return clearStr === reversed;
+// }
+
+// console.log(isPalindrome("racecar")); // true
+// console.log(isPalindrome("Race car")); // true
+// console.log(isPalindrome("hello")); // false
+
+function testHoisting() {
+  console.log(a);
+  var a = 10;
+
+  console.log(b);
+  let b = 20;
+
+  console.log(c);
+  const c = 30;
 }
 
-console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+console.log(testHoisting()); // ReferenceError: Cannot access 'b' before initialization
 
-function unique(arr) {
-  const result = [];
+console.log(sayHi());
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!result.includes(arr[i])) {
-      result.push(arr[i]);
-    }
-  }
+var sayHi = function () {
+  console.log("Привет от переменной!");
+};
 
-  return result;
+function sayHi() {
+  console.log("Привет от функции!");
 }
-
-console.log(unique([1, 2, 2, 3, 4, 4, 5]));
-
-function charFrequency(str) {
-  let result = {};
-
-  for (let i = 0; i < str.length; i++) {
-    if (result[str[i]]) {
-      result[str[i]] += 1;
-    } else {
-      result[str[i]] = 1;
-    }
-  }
-
-  return result;
-}
-
-console.log(charFrequency("hello"));
-
-function isPalindrome(str) {
-  const clearStr = str.toLowerCase().replace(/\s/g, "");
-
-  const reversed = clearStr.split("").reverse().join("");
-
-  return clearStr === reversed;
-}
-
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("Race car")); // true
-console.log(isPalindrome("hello")); // false
